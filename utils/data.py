26925f100e15570aac29516c4fd2d9e779b0422c
1seyoung <akdlrnal0705@gmail.com>
2025-02-18 09:09:06 +0900

    data.py

--- utils/data.py
+++ utils/data.py
@@ -1,3 +1,5 @@
+from .sheets import get_sheet_data
+
 def get_problems():
     """문제 목록 가져오기"""
     values = get_sheet_data('Problems', 'A2:D')
@@ -11,27 +13,3 @@ def get_problems():
                 'date_added': row[3]
             })
     return problems
-
-def save_submission(name, week, solution_link):
-    """제출 정보 저장"""
-    values = [
-        name,
-        week,
-        solution_link,
-        datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    ]
-    return append_row('Submissions', values)
-
-def get_submissions():
-    """제출 현황 가져오기"""
-    values = get_sheet_data('Submissions', 'A2:D')
-    submissions = []
-    for row in values:
-        if len(row) >= 4:
-            submissions.append({
-                '이름': row[0],
-                '주차': row[1],
-                '제출링크': row[2],
-                '제출시간': row[3]
-            })
-    return submissions
